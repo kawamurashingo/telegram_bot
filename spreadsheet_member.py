8448908638c4082248ad7458b7e4e9706d58740f
Kawamura Shingo <36791900+kawamurashingo@users.noreply.github.com>
2022-05-21 19:05:34 +0900

    Update spreadsheet_member.py

--- spreadsheet_member.py
+++ spreadsheet_member.py
@@ -12,7 +12,7 @@ client = gspread.authorize(creds)
 
 # Find a workbook by name and open the first sheet
 # Make sure you use the right name here.
-sheet = client.open("{SHEET NAME}").sheet1
+sheet = client.open("member").sheet1
 
 # Extract and print all of the values
 list_of_hashes = sheet.get_all_values()
